2773f12ec47dfa1fda8213bd675599d70b07adf7
Amir <amirsharifico@gmail.com>
2022-10-18 23:17:18 -0400

    18-Oct-2022

--- InventoryManagementSystem/window/win_add_edit_customer.xaml.cs
+++ InventoryManagementSystem/window/win_add_edit_customer.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DataModelLayer;
+using IMSBeta.Module;
 
 namespace IMSBeta.window
 {
@@ -22,6 +25,130 @@ namespace IMSBeta.window
         public win_add_edit_customer()
         {
             InitializeComponent();
+        }
+
+        public byte win_Type;
+        public string CName;
+        public string CAddress;
+        public string CTel;
+        public int CID;
+
+        inventorydbEntities1 database = new inventorydbEntities1();
+
+        private void image_close_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+        private void btn_exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Ok_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckNullable())
+            {
+                return;
+            }
+            //////////////////////////////////////////////////ثبت اطلاعات در دیتابیس
+            try
+            {
+
+                switch (win_Type)
+
+                {
+                    case 1:
+                        {///////Insert Block
+                            Customer C = new Customer();
+
+                            C.CustomerName = txt_customerName.Text.Trim();
+                            C.CustomerTel = txt_telcustomer.Text.Trim();
+                            C.CustomerAddress = txt_customerAddress.Text.Trim();
+                            C.UserID = PublicVariable.gUserId;
+
+                            database.Customers.Add(C);
+                            database.SaveChanges();
+
+                            MessageBox.Show("New customer added successfully");
+
+                            break;
+                        }
+                    case 2:
+                        {///////Update Block
+                            Customer Cu = (from C in database.Customers where C.CustomerID == this.CID select C).SingleOrDefault();
+
+                            Cu.CustomerName = txt_customerName.Text.Trim();
+                            Cu.CustomerTel = txt_telcustomer.Text.Trim();
+                            Cu.CustomerAddress = txt_customerAddress.Text.Trim();
+                            //  Cu.UpdateDate = lbl_date.Content.ToString();
+
+
+                            database.SaveChanges();
+
+                            MessageBox.Show("Customer information has been updated successfully");
+                            break;
+                        }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("There was a problem with the database. Please try again");
+            }
+            finally
+            {
+                this.Close();
+            }
+        }
+
+          
+        private bool CheckNullable()
+        {
+            if (txt_customerName.Text == "")
+            {
+                MessageBox.Show("Customer name is empty");
+                txt_customerName.Focus();
+                return false;
+            }
+            if (txt_telcustomer.Text == "")
+            {
+                MessageBox.Show("The customer's phone is empty");
+                txt_telcustomer.Focus();
+                return false;
+            }
+            if (txt_customerAddress.Text == "")
+            {
+                MessageBox.Show("Customer address is empty");
+                txt_customerAddress.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            txt_customerName.Focus();
+            lbl_username.Content = PublicVariable.gUserName + " " + PublicVariable.gUserFamily;
+
+            switch (win_Type)
+            {
+                case 2:
+                    txt_customerName.Text = this.CName;
+                    txt_customerAddress.Text = this.CAddress;
+                    txt_telcustomer.Text = this.CTel;
+
+                    ////////////
+                    break;
+            }
+        }
+
+        private void txt_telcustomer_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txt_telcustomer_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+"); e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
